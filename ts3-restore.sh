ebf32725eb3df65fd46aa524bec6517835361a02
Alex Palmer <xanderx@xanderx.com>
2016-03-26 23:24:21 +0000

    Stop server before restoring to prevent data inconsistency

--- ts3-restore.sh
+++ ts3-restore.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# Stop server before restoring to prevent data inconsistency
+docker stop teamspeak
 docker run -it --rm --volumes-from teamspeak-data --volume "$(pwd):/backup" teamspeak bash -c "find /data -mindepth 1 -depth -delete && tar -xvf \"/backup/${1}\" -C /"
+docker start teamspeak
 
